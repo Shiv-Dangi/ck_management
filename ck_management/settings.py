3c892a6eb72bd719d916e9f187a75f9df08a9a33
Shiv-Dangi <shiv.dangi71@gmail.com>
2017-02-07 02:38:53 -0500

    took backup from digital ocean droplet

--- ck_management/settings.py
+++ ck_management/settings.py
@@ -108,6 +108,6 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/var/www/ckmgindore.com/static/'
 STATIC_URL = '/static/'
